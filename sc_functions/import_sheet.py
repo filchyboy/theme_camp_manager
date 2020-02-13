0155480826127bc485fc572ca267d642ab4bbb33
Your name hereChristopher <christopher.filkins@gmail.com>
2020-02-13 11:51:05 -0800

    Import CSV works

--- sc_functions/import_sheet.py
+++ sc_functions/import_sheet.py
@@ -1,9 +1,10 @@
+import pandas as pd
 
-
-def import_google_sheet(url):
+def import_sheet(url):
     df = pd.read_csv(url)
-    return df
+    print(df)
 
 
 if __name__ == "__main__":
-    pass
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTMIUP6_JIoxWSAFCe1h6Hz12r-41t6qHv5cCXIBmYJUK2KS188pKkZnkr4jJRpIcC3mRZV36z21oNv/pub?gid=0&single=true&output=csv"
+    import_sheet(url)
