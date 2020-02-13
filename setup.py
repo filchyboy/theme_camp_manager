2ec38b8fa227f89f6697af34e00e8dbbe1745b8a
Your name hereChristopher <christopher.filkins@gmail.com>
2020-02-13 14:54:57 -0800

    Update

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="theme_camp_manager",
-    version="0.1.1",
+    version="0.1.2",
     author="Chris Filkins",
     author_email="christopher.filkins@gmail.com",
     description="Camp Manager Project for Theme Camps at Burning Man",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     license="MIT",
-    url="https://github.com/filchyboy/camp-manager",
+    url="https://github.com/filchyboy/theme_camp_manager",
     keywords="learning tools",
     packages=find_packages() # 
 )
