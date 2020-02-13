9367a9ca4cd5f7a56d659c81c083c8503fe74c66
Your name hereChristopher <christopher.filkins@gmail.com>
2020-02-13 06:55:42 -0800

    updated setup file

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="clf-lambdata-1",
+    name="camp-manager-sc",
     version="0.1.1",
     author="Chris Filkins",
     author_email="christopher.filkins@gmail.com",
-    description="Learning about my package",
+    description="Camp Manager Project for Theme Camps at Burning Man",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     license="MIT",
-    url="https://github.com/filchyboy/lambdata-pack",
+    url="https://github.com/filchyboy/camp-manager",
     keywords="learning tools",
-    packages=find_packages() # ["my_lambdata"]
+    packages=find_packages() # 
 )
